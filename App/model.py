23ee3fa9e405c13685bc156c9485a0ae05d0c082
sergioa2 <s.arangoa@uniandes.edu.co>
2021-02-06 15:15:51 -0500

    paso 4b pequeña correcion

--- App/model.py
+++ App/model.py
@@ -75,3 +75,5 @@ def createTagList():
 def addBookTags(booktagsfile):
     # TO-DO: Modificación de Est-1 y Est-2 en el Lab 2
     bts = lt.newList(datastructure='SINGLE_LINKED', filename=booktagsfile)
+    return bts
+    
