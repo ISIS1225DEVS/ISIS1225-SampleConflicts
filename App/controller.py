e3b446839740072722a27803ba296a0d9caf277b
ValeGoyeneche <m.goyenechea@uniandes.edu.co>
2021-02-08 18:11:11 -0500

    cambios estudiante 1

--- App/controller.py
+++ App/controller.py
@@ -52,6 +52,7 @@ def loadTags(filename):
     return tags
 
 
-def loadBooksTags(catalog):
-    # TO-DO: Modificación de Est-1 y Est-2 en el Lab 2
-    pass
+def loadBooksTags(filename): 
+# TO-DO: Modificación de Est-1 y Est-2 en el Lab 2 
+    booktagsfile = cf.data_dir + filename 
+    return model.addBooksTags(booktagsfile)
