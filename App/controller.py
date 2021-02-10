d425391b325a84b8a155910a74c6c2510d477e92
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-02-09 22:35:04 -0500

    cambios del estudiante 2

--- App/controller.py
+++ App/controller.py
@@ -52,6 +52,7 @@ def loadTags(filename):
     return tags
 
 
-def loadBooksTags(catalog):
-    # TO-DO: Modificación de Est-1 y Est-2 en el Lab 2
-    pass
+def loadBooksTags(filename):
+    btfile = cf.data_dir + filename
+    return model.addBooksTags(btfile)
+
