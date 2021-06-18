997e88ae2c2caf0ace2afac81a5c99a0ca7c2059
acastem15 <a.castellanosm@uniandes.edu.co>
2021-06-17 22:28:34 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -42,6 +42,8 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
+    print("3- Cargar Book-Tags!")
+    
     # TO-DO: Modificación de Est-1 en el Lab 2
     print("0- Salir")
 
