faadacadda1d5de3da2d0ac56b9b8dbc65896d96
José Daniel Montero Gutiérrez <j.monterog@uniandes.edu.co>
2021-08-20 14:36:31 -0500

    cambios del Estudiante 1

--- App/controller.py
+++ App/controller.py
@@ -52,6 +52,6 @@ def loadTags(filename):
     return tags
 
 
-def loadBooksTags(catalog):
-    # TODO: Modificación de Est-1 y Est-2 en el Lab 2
-    pass
+def loadBooksTags(filename):
+    booktagsfile = cf.data_dir + filename
+    return model.addBookTags(booktagsfile)
