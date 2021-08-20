0b720fd1030bf4cc48e799eca21f6c780a243bfd
Daniela Camacho <d.camachom@uniandes.edu.co>
2021-08-20 16:12:43 -0500

    cambios Estudiante 1

--- App/controller.py
+++ App/controller.py
@@ -54,4 +54,6 @@ def loadTags(filename):
 
 def loadBooksTags(catalog):
     # TODO: Modificación de Est-1 y Est-2 en el Lab 2
-    pass
+    def loadBooksTags(filename):
+        bookstagsfile = cf.data_dir + filename
+        return model.addBookTags(bookstagsfile)
