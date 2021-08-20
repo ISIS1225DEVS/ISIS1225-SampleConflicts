9b6f9318c12dd7cbb50dcce94b601bc1829d139a
SLatorre14 <s.latorre@uniandes.edu.co>
2021-08-20 10:45:47 -0500

    Cambios en Main

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Modificación de Est-1 en el Lab 2
+    print("3- Book tags")
     print("0- Salir")
 
 
