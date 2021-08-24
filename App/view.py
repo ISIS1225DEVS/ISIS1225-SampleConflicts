8f0093cab1d479d5c6c05c0436f84ae66206a960
Juan Lopez <je.lopezu1@uniandes.edu.co>
2021-08-24 17:28:25 -0500

    modificaciones estudiante 2

--- App/view.py
+++ App/view.py
@@ -91,7 +91,10 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
     elif True:
         # TODO: Modificación de Est-2 en el Lab 2
-        pass
+        elif int(inputs[0])==3
+            print ('Cargando información de Book-Tags...')
+            booktags=loadBookTags()
+            print ('Total de Book-Tags cargados: '+str(lt.size(booktags)))
 
     else:
         sys.exit(0)
