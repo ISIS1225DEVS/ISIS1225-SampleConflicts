e7da0f071bfa996f38c2440b9945d6d7028bb679
Giovanny <e.parra@uniandes.edu.co>
2021-08-25 22:16:01 -1200

    Cambios del estudiante 2

--- App/view.py
+++ App/view.py
@@ -65,6 +65,8 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TODO: Modificación de Est-1 en el Lab 2
+    print('Último libro cargado: ' + str(lt.lastElement(books)))
+
     pass
 
 
@@ -84,9 +86,14 @@ while True:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
+
         # TODO: Modificación de Est-2 en el Lab 2
-        pass
+
+    elif int(inputs[0]) == 3:
+        print("Cargando información de Book-Tags...")
+        booktags = loadBookTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
+        
 
     else:
         sys.exit(0)
